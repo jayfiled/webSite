1562e29492ea8a0a7df6014e2be6d76b13ae5f39
Jayfiled <jayfiled@gmail.com>
2018-06-30 21:28:06 +1000

    add default export to fix deploy error

--- src/pages/bluehyper.js
+++ src/pages/bluehyper.js
@@ -1,2 +1,14 @@
 //Going to be the page for the blue hyperlink tutorial
 
+import React from 'react'
+import Link from 'gatsby-link'
+import '../styles/blog-listing.css'
+import '../styles/layout-overide.css'
+
+const StringEx = () => (
+  <div>
+    <p> Need to export something or else the build fails </p>
+  </div>
+)
+
+export default StringEx
